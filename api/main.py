e343492d56a81c0f4c566b10744c344bf06f88b2
hamdiz0 <hamdizaghouan21166@gmail.com>
2024-12-20 20:23:10 +0100

    updated

--- api/main.py
+++ api/main.py
@@ -4,19 +4,15 @@ import qrcode
 from io import BytesIO
 import psycopg2
 import os
+import time
 from fastapi.responses import StreamingResponse
-import logging
 from dotenv import load_dotenv
-import os
 
-# Load environment variables
 load_dotenv()
 
 app = FastAPI()
 
-CORS_ORIGIN = os.getenv("CORS_ORIGIN") 
-
-app = FastAPI()
+CORS_ORIGIN = os.getenv("CORS_ORIGIN")
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,57 +22,73 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# PostgreSQL configuration
-db_host = os.getenv('POSTGRES_HOST')
-db_name = os.getenv('POSTGRES_DB')
-db_user = os.getenv('POSTGRES_USER')
-db_password = os.getenv('POSTGRES_PASSWORD')
-db_port = os.getenv('POSTGRES_PORT')
+# postgres configuration
+db_host = os.getenv("POSTGRES_HOST")
+db_name = os.getenv("POSTGRES_DB")
+db_user = os.getenv("POSTGRES_USER")
+db_password = os.getenv("POSTGRES_PASSWORD")
+db_port = os.getenv("POSTGRES_PORT")
 
-# Database connection
-try:
-    conn = psycopg2.connect(
-        host=db_host,
-        database=db_name,
-        user=db_user,
-        password=db_password,
-        port=db_port
-    )
-    logging.info("Database connection established successfully.")
-except Exception as e:
-    logging.error(f"Database connection failed: {e}")
+# wait for database creation
+def wait_for_database():
+    max_retries = 10
+    retry_interval = 5
+    retries = 0
 
-# Logging configuration
-logging.basicConfig(level=logging.INFO)
+    while retries < max_retries:
+        try:
+            conn = psycopg2.connect(
+                host=db_host,
+                database=db_name,
+                user=db_user,
+                password=db_password,
+                port=db_port,
+            )
+            print("Database connection established successfully.")
+            return conn
+        except Exception as e:
+            print(f"Database connection failed: {e}. Retrying in {retry_interval} seconds...")
+            retries += 1
+            time.sleep(retry_interval)
 
-# Check if the QR code exists
-def check_qr_code_exists(url: str):
+    raise Exception("Max retries exceeded. Database connection failed.")
+
+# connect to db
+conn = wait_for_database()
+
+# table creation
+def create_table():
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS qr_codes (
+        id SERIAL PRIMARY KEY,
+        url TEXT NOT NULL UNIQUE,  -- Ensure the URL is unique
+        qr_code_image BYTEA NOT NULL
+    );
+    """
     try:
         with conn.cursor() as cur:
-            cur.execute("SELECT qr_code_image FROM url_qr WHERE url = %s;", (url,))
-            result = cur.fetchone()
-            return result[0] if result else None
+            cur.execute(create_table_query)
+            conn.commit()
     except Exception as e:
-        logging.error(f"Error checking QR code existence: {e}")
-        return None
+        print(f"Table creation failed: {e}")
+        raise
+
+create_table()
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str = Query(..., description="The URL to encode in the QR code")):
-
-    # Check if the QR code already exists for the given URL
+    # check if qr code already exists
     with conn.cursor() as cur:
         cur.execute("SELECT qr_code_image FROM qr_codes WHERE url = %s;", (url,))
         existing_qr = cur.fetchone()
 
     if existing_qr:
-        # If it exists, return the existing QR code
-        print("it's in db ! ,retrieving ..." , existing_qr)
+        print("found in database. Retrieving...")
         img_byte_arr = BytesIO(existing_qr[0])
         img_byte_arr.seek(0)
         return StreamingResponse(img_byte_arr, media_type="image/png")
-    else :
-        print("it's not in db ! ,creating ...")
-        # Generate QR Code since it does not exist
+    else:
+        print("not found in database. Generating new one...")
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -88,24 +100,25 @@ async def generate_qr(url: str = Query(..., description="The URL to encode in th
 
         img = qr.make_image(fill_color="black", back_color="white")
         
-        # Save QR Code to BytesIO object
+        # save qr Code to BytesIO object
         img_byte_arr = BytesIO()
-        img.save(img_byte_arr, format='PNG')
+        img.save(img_byte_arr, format="PNG")
         img_byte_arr.seek(0)
 
-        logging.info(f"QR Code generated for URL: {url}, Image bytes length: {len(img_byte_arr.getvalue())}")
+        print(f"QR Code generated for URL: {url}, Image bytes length: {len(img_byte_arr.getvalue())}")
 
         try:
-            # Store QR code in PostgreSQL
+            # store qr code
             with conn.cursor() as cur:
-                cur.execute("""
+                cur.execute(
+                    """
                     INSERT INTO qr_codes (url, qr_code_image) VALUES (%s, %s) RETURNING id;
-                """, (url, img_byte_arr.getvalue()))
+                    """,
+                    (url, img_byte_arr.getvalue()),
+                )
                 conn.commit()
-
-            # Return the newly generated QR code
+            # return the generated qr code
             return StreamingResponse(img_byte_arr, media_type="image/png")
         except Exception as e:
-            logging.error(f"Database error: {e}")  # Log the error for debugging
+            print(f"Database error: {e}")
             raise HTTPException(status_code=500, detail=str(e))
-
