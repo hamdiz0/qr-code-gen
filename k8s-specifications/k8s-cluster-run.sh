4c3262b81da6d175e3562a5d55d94b07c42ad40c
hamdiz0 <hamdizaghouan21166@gmail.com>
2024-12-24 02:14:32 +0100

    updated

--- k8s-specifications/k8s-cluster-run.sh
+++ k8s-specifications/k8s-cluster-run.sh
@@ -16,7 +16,6 @@ echo "applying k8s yaml files"
 for file in "$DIR"/*.y*ml; do
     if [ -f "$file" ]; then
         echo "applying $file ..."
-        sed -i "s/<<VERSION>>/${VER}/" "$file" # swap the <<VERSION>> in the YAML files with the passed version VER
         kubectl apply -f "$file"
     else
         echo "no yaml files in : $DIR"
